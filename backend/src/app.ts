599fa40e4fb338d325734aca5a4003fe68d7c7c7
Valeriia Kharitonova <lerakharitonova2002@gmail.com>
2026-01-11 16:18:47 +0300

    Первая итерация 5

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -28,7 +28,8 @@ app.use(() => {
 app.use(errors());
 app.use(errorHandler);
 
-mongoose.connect('mongodb://127.0.0.1:27017/weblarek')
+const { DB_ADDRESS = 'mongodb://127.0.0.1:27017/weblarek' } = process.env;
+mongoose.connect(DB_ADDRESS);
   .then(() => {
     app.listen(3000, () => console.log('listening on port 3000'));
   })
